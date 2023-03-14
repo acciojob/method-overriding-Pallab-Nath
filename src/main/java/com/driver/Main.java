962abfb82e5f14cdf48e73cc84b635a369930750
pallab nath <you@example.com>
2023-03-14 12:48:07 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
-
 public class Main {
-  
+
+    public static void main(String args[])
+    {
+        B b = new B();
+        String s = b.meth();
+        System.out.println(s);
+    }
 }
